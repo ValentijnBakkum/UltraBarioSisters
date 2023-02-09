d6a9e8cbea0ef0195f4cdf2e5858fef7a58c3ad7
ValentijnBakkum <v.p.bakkum@student.tudelft.nl>
2023-02-09 02:47:46 +0100

    AudioManager Complete
    pog

---
+++ UltraBarioSisters/Assets/Sound.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound
+{
+    public string Name;
+    public AudioClip clip;
+
+    public bool loop;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(1f, 3f)]
+    public float pitch;
+
+    [HideInInspector]
+    public AudioSource source;
+}
